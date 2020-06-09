0a918358955be6dc2d4429b54194d882328a27ef
Sebastian Moreno Breser <sevmoreno@gmail.com>
2020-06-08 21:19:22 -0700

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Juan Moreno on 6/8/20.
+//  Copyright © 2020 Juan Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
